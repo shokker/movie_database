f4db004f37047aaf9a3fca53440189428f95708c
Lukas <lukas.danko1@gmail.com>
2018-08-28 09:28:05 +0200

    footer

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -10,26 +10,39 @@
         </title>
         <link href="{{ URL::asset('bootstrap/css/bootstrap.css') }}" rel="stylesheet">
         <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
-         
+
 
     </head>
     <body>
         <div class="container-fluid">
             <div class="jumbotron text-center">
                 <h1><a href="/">Movie database</a></h1>
-            </div> 
-    
+            </div>
+
             <div class="row">
                 @yield('content')
             </div>
+            <footer>
+              <div class="panel panel-default">
+                  <div class="panel-body">
+                    <div class="row">
+                      <div class="col col-xs-12 text-center">
+                        Created by Lukáš Danko @ 2016
+                      </div>
+                      <div class="col col-xs-12 text-center">
+                        <a href="https://github.com/shokker/movie_database"><i class="fa fa-github"> </i> project repozitory</a>
+                      </div>
+
+                    </div>
+
+                  </div>
+              </div>
+            </footer>
         </div>
-       
+
     </body>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <script src="bootstrap/js/bootstrap.min.js"></script>
 
     @include('partials/script')
 </html>
-
-
-
